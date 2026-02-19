cc1ba3fbe155c483198cd5af85868899b1975a1a
Nikhil <kujurnikhil0007@gmail.com>
2026-02-19 06:38:01 -0800

    minor mistack

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const  express = require('express');
 const  app = express();
-const  PORT = 3000
+
 app.get('/hello', (req, res) => {
   res.json({ message: "Hello World" });
 });
-app.listen(PORT , ()=> console.log(`server is running on port ${PORT}`));
+
 
 module.exports = app;
---
+++ index.js
@@ -0,0 +1,4 @@
+const  app = require('./app');
+
+PORT=3000
+app.listen(PORT , ()=> console.log(`server is running on port ${PORT}`));
