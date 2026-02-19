f129936ad96f14720014af8936432c6bdb51c8f5
Nikhil <kujurnikhil0007@gmail.com>
2026-02-19 06:20:50 -0800

    pipeline 1

---
+++ __tests__/app.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /hello', () => {
+  it('should return Hello World', async () => {
+    const res = await request(app).get('/hello');
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Hello World");
+  });
+});
